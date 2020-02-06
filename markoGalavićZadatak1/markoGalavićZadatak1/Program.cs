cd61c464aacb6e2363e127ef2277eeaa7ccd6177
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-02-06 11:02:13 +0100

    Add files via upload
    Zadatak 1 i 2

---
+++ markoGalavićZadatak1/markoGalavićZadatak1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b; 
+            float c;
+            float d;
+            float prosjek = 0;
+            Console.WriteLine("Unesi tri broja");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = a + b + c;
+            prosjek = d / 3;
+            Console.WriteLine("Prosjek je:" + prosjek);
+            Console.ReadKey();
+                
+
+        }
+    }
+}
